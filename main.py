e1e3ea67253658ff11d0892dcb9295ac82123627
wafflesncyanide <wafflesncyanide@gmail.com>
2025-07-11 03:02:43 -0500

    Spaceshiiiiiip

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import pygame # type: ignore
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    clockspeed = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     while True:
         screen.fill(000)
+        player.draw(screen)
+
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        
         pygame.display.flip()
+        dt = clockspeed.tick(60) / 1000
 
 
 if __name__ == "__main__":
