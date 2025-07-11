38c83d09527e96efefcdd5463709a463979cec17
wafflesncyanide <wafflesncyanide@gmail.com>
2025-07-11 04:02:49 -0500

    game over man! - added collision and loss mechanic

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,9 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collision(self, other):
+        if pygame.math.Vector2.distance_to(self.position,other.position) < (self.radius + other.radius):
+            return True
+        else:
+            return False
--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import pygame # type: ignore
+import sys
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     clockspeed = pygame.time.Clock()
     dt = 0
+    asteroids = pygame.sprite.Group()
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    AsteroidField.containers = (updateable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    Player.containers = (updateable, drawable)
+    asteroid_field = AsteroidField()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    
+
     while True:
         screen.fill(000)
-        player.update(dt)
-        player.draw(screen)
-
+        updateable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+        for rock in asteroids:
+            if rock.collision(player):
+                print("Game over!")
+                sys.exit()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
